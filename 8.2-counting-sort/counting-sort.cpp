8f3066e819ccd984efb9168ecbdf642de98b825a
Mario Admon <mladmon589@gmail.com>
2018-08-27 15:21:19 -0400

    Cleaned up the code: created a countingSort() function

--- 8.2-counting-sort/counting-sort.cpp
+++ 8.2-counting-sort/counting-sort.cpp
@@ -17,37 +17,8 @@ void print(const array<int,SIZE> &arr) {
 	cout << arr[arr.size()-1];
 }
 
-int main() {
-/*	string str;
-	int n = 20, k = 10;
-
-	cout << "Enter an array length, n: ";
-	getline(cin, str);
-	try {
-		n = stoi(str);
-	} catch (exception e) {
-		cout << "Entered invalid argument. Using default n = 20." << endl;
-	}
-
-	cout << "Enter an integer, k, such that k = O(n): ";
-	getline(cin, str);
-	try {
-		k = stoi(str);
-	} catch (exception e) {
-		cout << "Entered invalid argument. Using default k = 10." << endl;
-	} */
-
-	// Create an input array, A[0..n-1], with elements in the range 0-k
-	array<int, n> A;
-	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-	default_random_engine generator(seed);
-	uniform_int_distribution<int> distribution(0,k);
-	for (int &element : A) {
-		element = distribution(generator);
-	}
-
-	cout << "A["; print(A); cout << "]" << endl;
-
+template<size_t SIZE>
+void countingSort(const array<int, SIZE> &A, array<int, SIZE> &B) {
 	// Create an array, C[0..k], with all elements initialized to 0
 	array<int, k+1> C = {};
 
@@ -55,23 +26,35 @@ int main() {
 	for (int i=0; i < A.size(); i++) {
 		C[A[i]]++;
 	}
-
 	cout << "C["; print(C); cout << "]" << endl;
 
 	// Let C[i] contain the number of elements less than or equal to i
 	for (int i=1; i < C.size(); i++) {
 		C[i]+=C[i-1];
 	}
-
 	cout << "C["; print(C); cout << "]" << endl;
 
-	// Create an array, B[0..n-1] that contanis the elements of A in sorted order
-	array<int, n> B;
+
 	for (int i = A.size()-1; i >= 0; i--) {
 		B[C[A[i]]-1] = A[i];
 		C[A[i]]--;
 	}
+}
 
+int main() {
+	// Create an input array, A[0..n-1], with elements in the range 0-k
+	array<int, n> A;
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed);
+	uniform_int_distribution<int> distribution(0,k);
+	for (int &element : A) { // range-based for loop: each element is an int &
+		element = distribution(generator);
+	}
+	cout << "A["; print(A); cout << "]" << endl;
+
+	// Create an array, B[0..n-1] that contanis the elements of A in sorted order
+	array<int, n> B;
+	countingSort(A, B);
 	cout << "B["; print(B); cout << "]" << endl;
 
 	return 0;
