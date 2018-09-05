1254a785388b377662d287a3418b96f2abfab18c
Mario Admon <mladmon589@gmail.com>
2018-09-05 15:23:41 -0400

    Modified to use vectors and unordered_sets

--- 9.2-selection/select.cpp
+++ 9.2-selection/select.cpp
@@ -3,34 +3,36 @@
 #include <exception>
 #include <chrono>
 #include <random>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-void partition(vector<int> &v, int p, int r) {
+void partition(unordered_set<int> &s, int p, int r) {
 
 }
 
 int random(int p, int r) {
-	return 0;
+
 }
 
-void randomizedPartition(vector<int> &v, int p, int r) {
+void randomizedPartition(unordered_set<int> &s, int p, int r) {
 
 }
 
 /* Return the ith order statistic from a set of n distinct numbers (i.e., the
    ith smallest number) */
-int randomizedSelect(vector<int> &v, int p, int r, int i) {
-	return 0;
+int randomizedSelect(unordered_set<int> &s, int p, int r, int i) {
+
 }
 
 int main() {
-	// Generate a set A[0..n-1] of n distinct numbers
+	// Generate a set S[0..n-1] of n distinct numbers
+	
 
 	// Ask the user for an integer i in the range [1-n]
 
-	// Call randomizedSelect() to find the ith smallest number in A[0..n-1]
+	// Call randomizedSelect() to find the ith smallest number in S[0..n-1]
 
 	return 0;
 }
