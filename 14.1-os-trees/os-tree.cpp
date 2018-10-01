38030544b66ed1fe94bd9e0dde070076910c6016
Mario Admon <mladmon589@gmail.com>
2018-10-01 16:47:22 -0400

    Implemented OS_Tree::osSelect(), OS_Tree::osRank()

--- 14.1-os-trees/os-tree.cpp
+++ 14.1-os-trees/os-tree.cpp
@@ -17,13 +17,26 @@ void OS_Tree::rbDelete(Node *z) {
 }
 
 Node *OS_Tree::osSelect(Node *x, int i) {
-	// To-Do: Implement this method
-
-	return nil;
+	int rank = x->left->size + 1;
+	if (i == rank) {
+		return x;
+	} else if (i < rank) {
+		return osSelect(x->left, i);
+	} else {
+		return osSelect(x->right, i-rank);
+	}
 }
 
 int OS_Tree::osRank(Node *x) {
-	// To-Do: Implement this method
+	int rank = x->left->size + 1;
+	Node *y = x->p;
+	while (y != nil) {
+		if (x == y->right) {
+			rank += y->left->size + 1;
+		}
+		x = y;
+		y = y->p;
+	}
 
-	return 0;
+	return rank;
 }
