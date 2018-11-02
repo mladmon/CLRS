a9af5de3395067333be737a97d578fc090a39764
Mario Admon <mladmon589@gmail.com>
2018-11-02 15:39:33 -0400

    Implemented a much better huffman() that uses dynamic memory

--- 16.3-huffman-codes/huffman-codes.cpp
+++ 16.3-huffman-codes/huffman-codes.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <functional>		// std::hash
+#include <vector>
 #include <unordered_set>	// std::unordered_set
 #include <queue>				// std::priority_queue
 
@@ -9,116 +9,72 @@ class Node {
 		char c;
 		int freq;
 	public:
-		const Node *left, *right;
-		Node(int freq) : c('\0'), freq(freq) {}
-		Node(char c, int freq) : c(c), freq(freq) {}
-		// needed for Node's use in unordered_set container
-		// note: added comparison between frequencies as well to make this implementation of Huffman() work... will fix
-		bool operator==(const Node &other) const { return (c == other.c) && freq == other.freq; }
-		// needed for Node's use in priority_queue container
-		// note: implemented as freq > other.freq to implement min priority_queue!
-		bool operator<(const Node &other) const { return freq > other.freq; }
+		Node *left, *right;
+		Node(int freq) : c('_'), freq(freq) {}
+		Node(char c, int freq) : c(c), freq(freq), left(NULL), right(NULL) {}
 		char getChar() const { return c; }
 		int getFreq() const { return freq; }
 };
 
-// needed for Node's use in unordered_set container
-template <> struct std::hash<Node> {
-	size_t operator()(const Node &n) const {
-		return hash<char>()(n.getChar());
+// needed for priority_queue<Node *> container Compare template parameter
+// note the use of '>' in order to implement a min priority_queue
+struct CompareNodePtr {
+	bool operator()(const Node *pt1, const Node *pt2) const {
+		return pt1->getFreq() > pt2->getFreq();
 	}
 };
 
-/* This bad implementatin of huffman() uses memory on the stack instead of
-   dynamically allocated memory, so we need to be careful when assigning z.left
-   and z.right. They need to point to the addresses of corresponding nodes in
-   C, not Q since we're copying and destroying Nodes quite often. this
-   implementation adds newly created z nodes to C as well, so their addresses
-   are used to assign subsequent z.left and z.right... this is bad I know. I
-   also had to modify the operator== to include checking frequencies because
-   I'm now inserting multiple characters into C with c set to '\0'! Next version
-   will use pointers and pass the function a min priority_queue from main().
-   This was a good learning experience for how memory works and which addresses
-   to use... you really have to understand what's going on to make this bad
-   version work.
-*/
-Node huffman(unordered_set<Node> &C) {
-	int n = C.size();
-
-	priority_queue<Node> Q;
-	for (const Node &c : C) {
+Node * huffman(const unordered_set<Node *> &C) {
+	priority_queue<Node *, vector<Node *>, CompareNodePtr> Q;
+	for (Node *c : C) {
 		Q.push(c);
 	}
 
-	for (int i=1; i <= n-1; i++) {
-		Node x = Q.top();
+	for (unsigned i=0; i < C.size()-1; i++) {
+		Node *x = Q.top();
 		Q.pop();
-		Node y = Q.top();
+		Node *y = Q.top();
 		Q.pop();
 
-		Node z(x.getFreq() + y.getFreq());
-		z.left = &(*(C.find(x)));
-		z.right = &(*(C.find(y)));
-		C.insert(z);
+		Node *z = new Node(x->getFreq() + y->getFreq());
+		z->left = x;
+		z->right = y;
 		Q.push(z);
 	}
 
-	cout << "Q.size(): " << Q.size() << endl;
 	return Q.top();
 }
 
+void printNodes(Node *np) {
+	if (np != NULL) {
+		cout << np->getChar() << ":" << np->getFreq() << endl;
+		printNodes(np->left);
+		printNodes(np->right);
+	}
+}
+
+void deleteNodes(Node *np) {
+	if (np != NULL) {
+		deleteNodes(np->left);
+		deleteNodes(np->right);
+		cout << "Deleting " << np->getChar() << ":" << np->getFreq() << endl;
+		delete np;
+	}
+}
+
 int main() {
-	unordered_set<Node> C({
-		Node('a', 45),
-		Node('b', 13),
-		Node('c', 12),
-		Node('d', 16),
-		Node('e', 9),
-		Node('f', 5)
+	unordered_set<Node *> C({
+		new Node('a', 45),
+		new Node('b', 13),
+		new Node('c', 12),
+		new Node('d', 16),
+		new Node('e', 9),
+		new Node('f', 5)
 	});
 
-/*	unordered_set<Node>::const_iterator it = C.find(Node('b', 13));
-	if (it == C.end()) {
-		cout << "Character not found!" << endl;
-	} else {
-		cout << "Character is in the set!" << endl;
-		cout << "c: " << it->getChar() << endl;
-		cout << "address: " << &(*it) << endl;
-	}*/
-
-	cout << "C.size(): " << C.size() << endl;
-	Node root = huffman(C);
-	cout << "C.size(): " << C.size() << endl;
-
-	// depth 0
-	cout << "root.getFreq(): " << root.getFreq() << endl;
-
-	// depth 1
-	cout << "root.left->getChar(): " << root.left->getChar() << endl;
-	cout << "root.left->getFreq(): " << root.left->getFreq() << endl;
-	cout << "root.right->getFreq(): " << root.right->getFreq() << endl;
-
-	// depth 2
-	cout << "root.right->left->getFreq(): " << root.right->left->getFreq() << endl;
-	cout << "root.right->right->getFreq(): " << root.right->right->getFreq() << endl;
-
-
-	// depth 3
-	cout << "root.right->left->left->getchar(): " << root.right->left->left->getChar() << endl;
-	cout << "root.right->left->left->getFreq(): " << root.right->left->left->getFreq() << endl;
-	cout << "root.right->left->right->getchar(): " << root.right->left->right->getChar() << endl;
-	cout << "root.right->left->right->getFreq(): " << root.right->left->right->getFreq() << endl;
-
-	cout << "root.right->right->left->getFreq(): " << root.right->right->left->getFreq() << endl;
-	cout << "root.right->right->right->getchar(): " << root.right->right->right->getChar() << endl;
-	cout << "root.right->right->right->getFreq(): " << root.right->right->right->getFreq() << endl;
-
-	// depth 4
-	cout << "root.right->right->left->left->getChar(): " << root.right->right->left->left->getChar() << endl;
-	cout << "root.right->right->left->left->getFreq(): " << root.right->right->left->left->getFreq() << endl;
-
-	cout << "root.right->right->left->right->getChar(): " << root.right->right->left->right->getChar() << endl;
-	cout << "root.right->right->left->right->getFreq(): " << root.right->right->left->right->getFreq() << endl;
+	Node *root = huffman(C);
+	printNodes(root);
+	deleteNodes(root);
 
 	return 0;
 }
